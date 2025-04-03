11190f9dfc6b84c1b09ea520189fe154bee733ed
unknown <joacodileo@gmail.com>
2025-04-03 18:40:35 -0300

    variables para deploy

--- server/src/db.js
+++ server/src/db.js
@@ -2,15 +2,33 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/sportquatro`, {
+/* const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/sportquatro`, {
   logging: false,
   native: false,
   define: {
     timestamps: false,
   },
-});
+}); */
+
+//deploy
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+  {
+    logging: false,
+    native: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    define: {
+      timestamps: false,
+    },
+  },
+);
 
 const basename = path.basename(__filename);
 
