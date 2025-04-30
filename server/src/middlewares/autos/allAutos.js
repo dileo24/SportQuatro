a1e4c9f76bf8bfbe1f2217c5840b55ad6691b732
unknown <joacodileo@gmail.com>
2025-04-30 18:57:58 -0300

    timestamp false

--- server/src/middlewares/autos/allAutos.js
+++ server/src/middlewares/autos/allAutos.js
@@ -10,7 +10,7 @@ const allAutos = async (req, res, next) => {
           through: { attributes: [] },
         },
       ],
-      order: ["id", "ASC"],
+      order: [["id", "ASC"]],
     });
 
     res.status(200).json({
